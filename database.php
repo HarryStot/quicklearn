b97930daeeb2b28be83df24c41947fbbfd1c2c02
Nathan <bourguignon.nathan@gmail.com>
2020-05-02 10:20:03 +0200

    Modify init and database

--- database.php
+++ database.php
@@ -1,1 +1,15 @@
-// to do
+<?php
+    $servername = "localhost";
+    $username = "quicdpsf_root";
+    $password = "8!m9MreUxr!5xSw";
+    $dbname = "quicdpsf_db";
+    $port = "3306";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
--- init.php
+++ init.php
@@ -1,1 +1,18 @@
-// to do
+<?php
+    session_start();
+    if (isset($_COOKIE['conn'])) {
+        if ($_COOKIE['conn'] == "on") {
+            // ok
+            // TODO: problem cookies can be change. Need put value of cookie = value of session
+        } else {
+            header('Location: https://quicklearn.yj.fr');
+            exit();
+        }
+    } else {
+        header('Location: https://quicklearn.yj.fr');
+        exit();
+    }
+
+    $usPseudo = $_SESSION['pseudo'];
+    $id = $_SESSION['id'];
+?>
