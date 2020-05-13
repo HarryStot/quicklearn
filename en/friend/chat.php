0cb8cea08ed75d43be62f1037fccc1dd0c11136e
HarryStot <bourguignon.natha@gmail.com>
2020-05-13 10:39:15 +0200

    add style for chat

--- en/friend/chat.php
+++ en/friend/chat.php
@@ -51,7 +51,7 @@ global $id, $usPseudo, $conn;
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="../../style/styleChat.css" rel="stylesheet" type="text/css">
     <title>Chat with your friend - QuickLearn</title>
     <link rel="icon" type="image/png" href="https://i.ibb.co/99nFZkB/qlmin.png"/>
 </head>
