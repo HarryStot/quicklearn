4bbde31e7824dea0e7bacecd894f39977e64666d
HarryStot <bourguignon.natha@gmail.com>
2020-05-14 19:14:38 +0200

    add select in chat

--- en/friend/chat.php
+++ en/friend/chat.php
@@ -1,21 +1,21 @@
 <?php
-    include "../../init.php";
-    include '../../database.php';
-    global $id, $usPseudo, $conn;
+include "../../init.php";
+include '../../database.php';
+global $id, $usPseudo, $conn;
 
-    /* query sql pour récup les noms et id des amis */
-    $sql_reqFriend = "SELECT * FROM friend JOIN users ON (friend.id_user1 = users.id OR friend.id_user2 = users.id) AND users.id != '$id' GROUP BY id";
-    $res_reqFriend = $conn->query($sql_reqFriend);       // on utilise JOIN pour combiner les deux tables de notre db(database)     |-> cette commande permet
-    if ($res_reqFriend->num_rows > 0) {                                                 //                                           de ne pas prendre soi même
-        while ($infoFriend = $res_reqFriend->fetch_array()) {
-            $arrFriendName[] = $infoFriend['pseudo']; // créer deux lists qui contiènne tout les amis + les id (on pourait aussi utiliser le format JSON -> (on utilise json_encode() pour transmettre
-            $arrFriendId[] = $infoFriend['id'];                                  //                                                               les donnés de pHp à JavaScript ))
-        }
-    } else {
-        // no friend
+/* query sql pour récup les noms et id des amis */
+$sql_reqFriend = "SELECT * FROM friend JOIN users ON (friend.id_user1 = users.id OR friend.id_user2 = users.id) AND users.id != '$id' GROUP BY id";
+$res_reqFriend = $conn->query($sql_reqFriend);       // on utilise JOIN pour combiner les deux tables de notre db(database)     |-> cette commande permet
+if ($res_reqFriend->num_rows > 0) {                                                 //                                           de ne pas prendre soi même
+    while ($infoFriend = $res_reqFriend->fetch_array()) {
+        $arrFriendName[] = $infoFriend['pseudo']; // créer deux lists qui contiènne tout les amis + les id (on pourait aussi utiliser le format JSON -> (on utilise json_encode() pour transmettre
+        $arrFriendId[] = $infoFriend['id'];                                  //                                                               les donnés de pHp à JavaScript ))
     }
+} else {
+    // no friend
+}
 
-    $conn->close();
+$conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="en" onload="showMiniLaps()">
@@ -77,12 +77,13 @@
             let div1 = document.createElement("DIV");
             div1.setAttribute('class', 'list-friend');
             // add background color heres
+            div1.setAttribute('id', arrIdFr[i]);
             ulFr.appendChild(div1);
 
             let btn = document.createElement("BUTTON");
             btn.setAttribute('class', 'btn btn-friend');
             btn.setAttribute('onfocus', 'this.blur()');
-            // add on click change fiend
+            // add on click change friend
             btn.setAttribute('onclick', 'changeFr(' + arrIdFr[i] + ')');
             div1.appendChild(btn);
 
@@ -99,8 +100,24 @@
     }
 
     function changeFr(newIdFr) {
-        idFr = newIdFr;
-        show();
+        if (idFr != newIdFr) {
+            idFr = newIdFr;
+            show();
+
+
+            let element = document.getElementsByClassName("select");
+            if (element.length > 0) {
+                for (let i=0; i < element.length; i++) {
+                    element[0].classList.remove("select");
+                }
+            }
+
+
+
+            let select = document.getElementById(newIdFr);
+            select.classList.add('select');
+            show();
+        }
     }
 
     var tableMes = document.getElementById("table-body");
@@ -109,56 +126,60 @@
         destruct(); // suprime les anciens message pour ne pas faire de duplication
         getMessage(); // récup les messages dans la base de données
 
-        if (message.length) {
-            /* cette function fait :
-            * - appelle une autre function qui va supprimer les messages
-            * - appelle une autre function qui va récup les messages dans la db
-            * - affiche les messages avec des élements HTML pour que ce soit agréable
-            * - appelle une autre function qui va reappelle cette function 3s plus tard
-            */
-            for (i = 0; i < message.length; i++) {
-                /* créer élement <tr> */
-                let a = document.createElement("TR");
+        if (message === null) {
+            getMessage();
+        } else {
+            if (message.length) {
+                /* cette function fait :
+                * - appelle une autre function qui va supprimer les messages
+                * - appelle une autre function qui va récup les messages dans la db
+                * - affiche les messages avec des élements HTML pour que ce soit agréable
+                * - appelle une autre function qui va reappelle cette function 3s plus tard
+                */
+                for (i = 0; i < message.length; i++) {
+                    /* créer élement <tr> */
+                    let a = document.createElement("TR");
 
-                /*append the element as a child of the tr: */
-                tableMes.appendChild(a);
+                    /*append the element as a child of the tr: */
+                    tableMes.appendChild(a);
 
-                let b = document.createElement("TD");
-                if (message[i][1] == id) {
-                    // message from me
-                    let cR = document.createElement("td");
+                    let b = document.createElement("TD");
+                    if (message[i][1] == id) {
+                        // message from me
+                        let cR = document.createElement("td");
 
-                    let dR = document.createElement("div");
-                    dR.setAttribute('class', 'message right');
-                    cR.appendChild(dR);
+                        let dR = document.createElement("div");
+                        dR.setAttribute('class', 'message right');
+                        cR.appendChild(dR);
 
-                    let eR = document.createElement("div");
-                    eR.setAttribute('class', 'text-mess');
-                    dR.appendChild(eR);
+                        let eR = document.createElement("div");
+                        eR.setAttribute('class', 'text-mess');
+                        dR.appendChild(eR);
 
-                    let fR = document.createElement("span");
-                    eR.appendChild(fR);
+                        let fR = document.createElement("span");
+                        eR.appendChild(fR);
 
-                    fR.innerHTML += message[i][3];
-                    a.appendChild(b);
-                    a.appendChild(cR);
-                } else {
-                    // message from friend
-                    let dL = document.createElement("div");
-                    dL.setAttribute('class', 'message left');
-                    b.appendChild(dL);
+                        fR.innerHTML += message[i][3];
+                        a.appendChild(b);
+                        a.appendChild(cR);
+                    } else {
+                        // message from friend
+                        let dL = document.createElement("div");
+                        dL.setAttribute('class', 'message left');
+                        b.appendChild(dL);
 
-                    let eL = document.createElement("div");
-                    eL.setAttribute('class', 'text-mess');
-                    dL.appendChild(eL);
+                        let eL = document.createElement("div");
+                        eL.setAttribute('class', 'text-mess');
+                        dL.appendChild(eL);
 
-                    let fL = document.createElement("span");
-                    eL.appendChild(fL);
+                        let fL = document.createElement("span");
+                        eL.appendChild(fL);
 
-                    fL.innerHTML += message[i][3];
-                    a.appendChild(b);
+                        fL.innerHTML += message[i][3];
+                        a.appendChild(b);
+                    }
+
                 }
-
             }
         }
         showLapse();
